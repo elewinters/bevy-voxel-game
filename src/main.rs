a7bbbfc2769a94983bb4f362934d93d9467e4625
ele <86350819+elewinters@users.noreply.github.com>
2025-07-28 19:35:45 +0200

    testing perlin noise terrain generation

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use bevy::color::palettes::css::*;
 
 use bevy_rapier3d::{control::KinematicCharacterController, prelude::*};
 
+use noise::*;
+
 mod player;
 mod window;
 
@@ -34,25 +36,31 @@ fn spawn_map(
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
     // ground
-    commands.spawn((
-        Transform::from_xyz(0.0, -0.1, 0.0),
-        Collider::cuboid(50.0, 0.1, 50.0),
-    ));
+    //commands.spawn((
+    //    Transform::from_xyz(0.0, -0.1, 0.0),
+    //    Collider::cuboid(50.0, 0.1, 50.0),
+    //));
 
     // spawn a plane of cubes in 3 dimensions
+    let noise = Perlin::new(512);
 
-    let size_x = 32;
-    let size_y = 5;
-    let size_z = 32;
+    let size_x = 32 * 3;
+    let size_y = 12;
+    let size_z = 32 * 3;
 
     for x in 0..size_x {
         for y in 0..size_y {
             for z in 0..size_x {
+                let flatness = 20.0; // the higher the number, the less hills there are
+                let spikiness = 10.0; // the higher the number, the spikier hills get
+
+                let noise_y = noise.get([x as f64 / flatness , y as f64 / flatness , z as f64 / flatness]) as f32 * spikiness;               
+
                 if x == 0 || x == size_x - 1 ||
                    y == 0 || y == size_y - 1 ||
                    z == 0 || z == size_z - 1 {
                     commands.spawn((
-                        Transform::from_xyz(x as f32, y as f32, z as f32),
+                        Transform::from_xyz(x as f32, noise_y, z as f32),
 
                         Mesh3d(meshes.add(Cuboid::new(1.0, 1.0, 1.0))),
                         Collider::cuboid(0.5, 0.5, 0.5),
