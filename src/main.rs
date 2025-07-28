1d96b4c0ab17c92af781d8a2ebfd2ad965c02eb8
ele <86350819+elewinters@users.noreply.github.com>
2025-07-28 15:37:41 +0200

    added a collidable cube to the scene

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use bevy::prelude::*;
+use bevy::color::palettes::css::*;
+
 use bevy_rapier3d::{control::KinematicCharacterController, prelude::*};
 
 mod player;
@@ -26,38 +28,24 @@ fn main() {
 /* ---------------- */
 /*      systems     */
 /* ---------------- */
-fn spawn_map(mut commands: Commands) {
+fn spawn_map(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
     // ground
-    let ground_size = 50.0;
-    let ground_height = 0.1;
-
     commands.spawn((
-        Transform::from_xyz(0.0, -ground_height, 0.0),
-        Collider::cuboid(ground_size, ground_height, ground_size),
+        Transform::from_xyz(0.0, -0.1, 0.0),
+        Collider::cuboid(50.0, 0.1, 50.0),
     ));
 
-    // stairs
-    let stair_len = 30;
-    let stair_step = 1.0;
+    // cube
+    commands.spawn((
+        Transform::from_xyz(0.0, 0.5, 0.0),
 
-    for i in 1..=stair_len {
-        let step = i as f32;
-        let collider = Collider::cuboid(1.0, step * stair_step, 1.0);
-        commands.spawn((
-            Transform::from_xyz(40.0, step * stair_step, step * 2.0 - 20.0),
-            collider.clone(),
-        ));
-        commands.spawn((
-            Transform::from_xyz(-40.0, step * stair_step, step * -2.0 + 20.0),
-            collider.clone(),
-        ));
-        commands.spawn((
-            Transform::from_xyz(step * 2.0 - 20.0, step * stair_step, 40.0),
-            collider.clone(),
-        ));
-        commands.spawn((
-            Transform::from_xyz(step * -2.0 + 20.0, step * stair_step, -40.0),
-            collider.clone(),
-        ));
-    }
+        Mesh3d(meshes.add(Cuboid::new(1.0, 1.0, 1.0))),
+        Collider::cuboid(0.5, 0.5, 0.5),
+
+        MeshMaterial3d(materials.add(Color::from(GREEN))),
+    ));
 }
