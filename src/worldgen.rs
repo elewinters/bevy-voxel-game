10a8aa1d4d421fc3078036f1a48cf27443c79115
ele <86350819+elewinters@users.noreply.github.com>
2025-07-29 14:49:01 +0200

    im gonna roll this create_cube_mesh stuff back it doesnt help with performance that much

--- src/worldgen.rs
+++ src/worldgen.rs
@@ -18,14 +18,14 @@ impl Plugin for WorldGenPlugin {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 enum BlockFace {
-    Top,
-    Bottom,
-    Right,
-    Left,
-    Back,
-    Forward
+    Top = 0,
+    Bottom = 1,
+    Right = 2,
+    Left = 3,
+    Back = 4,
+    Forward = 5
 }
 
 /* ------------------ */
@@ -47,108 +47,129 @@ fn generate_noise(x: f64, y: f64, z: f64) -> f32 {
 fn create_cube_mesh<const N: usize>(without_faces: [BlockFace; N]) -> Mesh {
     let mut mesh = Mesh::new(PrimitiveTopology::TriangleList, RenderAssetUsages::RENDER_WORLD);
 
-    mesh.insert_attribute(
-        Mesh::ATTRIBUTE_POSITION,
-        // Each array is an [x, y, z] coordinate in local space.
-        // The camera coordinate space is right-handed x-right, y-up, z-back. This means "forward" is -Z.
-        // Meshes always rotate around their local [0, 0, 0] when a rotation is applied to their Transform.
-        // By centering our mesh around the origin, rotating the mesh preserves its center of mass.
-        vec![
-            // top (facing towards +y)
-            [-0.5, 0.5, -0.5], // vertex with index 0
-            [0.5, 0.5, -0.5], // vertex with index 1
-            [0.5, 0.5, 0.5], // etc. until 23
-            [-0.5, 0.5, 0.5],
-            // bottom   (-y)
-            [-0.5, -0.5, -0.5],
-            [0.5, -0.5, -0.5],
-            [0.5, -0.5, 0.5],
-            [-0.5, -0.5, 0.5],
-            // right    (+x)
-            [0.5, -0.5, -0.5],
-            [0.5, -0.5, 0.5],
-            [0.5, 0.5, 0.5], // This vertex is at the same position as vertex with index 2, but they'll have different UV and normal
-            [0.5, 0.5, -0.5],
-            // left     (-x)
-            [-0.5, -0.5, -0.5],
-            [-0.5, -0.5, 0.5],
-            [-0.5, 0.5, 0.5],
-            [-0.5, 0.5, -0.5],
-            // back     (+z)
-            [-0.5, -0.5, 0.5],
-            [-0.5, 0.5, 0.5],
-            [0.5, 0.5, 0.5],
-            [0.5, -0.5, 0.5],
-            // forward  (-z)
-            [-0.5, -0.5, -0.5],
-            [-0.5, 0.5, -0.5],
-            [0.5, 0.5, -0.5],
-            [0.5, -0.5, -0.5],
-        ],
-    );
+    let mut positions = vec![
+        // top (facing towards +y)
+        [-0.5, 0.5, -0.5], // vertex with index 0
+        [0.5, 0.5, -0.5], // vertex with index 1
+        [0.5, 0.5, 0.5], // etc. until 23
+        [-0.5, 0.5, 0.5],
+        // bottom   (-y)
+        [-0.5, -0.5, -0.5],
+        [0.5, -0.5, -0.5],
+        [0.5, -0.5, 0.5],
+        [-0.5, -0.5, 0.5],
+        // right    (+x)
+        [0.5, -0.5, -0.5],
+        [0.5, -0.5, 0.5],
+        [0.5, 0.5, 0.5], // This vertex is at the same position as vertex with index 2, but they'll have different UV and normal
+        [0.5, 0.5, -0.5],
+        // left     (-x)
+        [-0.5, -0.5, -0.5],
+        [-0.5, -0.5, 0.5],
+        [-0.5, 0.5, 0.5],
+        [-0.5, 0.5, -0.5],
+        // back     (+z)
+        [-0.5, -0.5, 0.5],
+        [-0.5, 0.5, 0.5],
+        [0.5, 0.5, 0.5],
+        [0.5, -0.5, 0.5],
+        // forward  (-z)
+        [-0.5, -0.5, -0.5],
+        [-0.5, 0.5, -0.5],
+        [0.5, 0.5, -0.5],
+        [0.5, -0.5, -0.5],
+    ];
 
-    mesh.insert_attribute(
-        Mesh::ATTRIBUTE_UV_0,
-        vec![
-            // Assigning the UV coords for the top side.
-            [0.0, 0.2], [0.0, 0.0], [1.0, 0.0], [1.0, 0.2],
-            // Assigning the UV coords for the bottom side.
-            [0.0, 0.45], [0.0, 0.25], [1.0, 0.25], [1.0, 0.45],
-            // Assigning the UV coords for the right side.
-            [1.0, 0.45], [0.0, 0.45], [0.0, 0.2], [1.0, 0.2],
-            // Assigning the UV coords for the left side.
-            [1.0, 0.45], [0.0, 0.45], [0.0, 0.2], [1.0, 0.2],
-            // Assigning the UV coords for the back side.
-            [0.0, 0.45], [0.0, 0.2], [1.0, 0.2], [1.0, 0.45],
-            // Assigning the UV coords for the forward side.
-            [0.0, 0.45], [0.0, 0.2], [1.0, 0.2], [1.0, 0.45],
-        ],
-    );
+    let mut uvs = vec![
+        // Assigning the UV coords for the top side.
+        [0.0, 0.2], [0.0, 0.0], [1.0, 0.0], [1.0, 0.2],
+        // Assigning the UV coords for the bottom side.
+        [0.0, 0.45], [0.0, 0.25], [1.0, 0.25], [1.0, 0.45],
+        // Assigning the UV coords for the right side.
+        [1.0, 0.45], [0.0, 0.45], [0.0, 0.2], [1.0, 0.2],
+        // Assigning the UV coords for the left side.
+        [1.0, 0.45], [0.0, 0.45], [0.0, 0.2], [1.0, 0.2],
+        // Assigning the UV coords for the back side.
+        [0.0, 0.45], [0.0, 0.2], [1.0, 0.2], [1.0, 0.45],
+        // Assigning the UV coords for the forward side.
+        [0.0, 0.45], [0.0, 0.2], [1.0, 0.2], [1.0, 0.45],
+    ];
 
-    mesh.insert_attribute(
-        Mesh::ATTRIBUTE_NORMAL,
-        vec![
-            // Normals for the top side (towards +y)
-            [0.0, 1.0, 0.0],
-            [0.0, 1.0, 0.0],
-            [0.0, 1.0, 0.0],
-            [0.0, 1.0, 0.0],
-            // Normals for the bottom side (towards -y)
-            [0.0, -1.0, 0.0],
-            [0.0, -1.0, 0.0],
-            [0.0, -1.0, 0.0],
-            [0.0, -1.0, 0.0],
-            // Normals for the right side (towards +x)
-            [1.0, 0.0, 0.0],
-            [1.0, 0.0, 0.0],
-            [1.0, 0.0, 0.0],
-            [1.0, 0.0, 0.0],
-            // Normals for the left side (towards -x)
-            [-1.0, 0.0, 0.0],
-            [-1.0, 0.0, 0.0],
-            [-1.0, 0.0, 0.0],
-            [-1.0, 0.0, 0.0],
-            // Normals for the back side (towards +z)
-            [0.0, 0.0, 1.0],
-            [0.0, 0.0, 1.0],
-            [0.0, 0.0, 1.0],
-            [0.0, 0.0, 1.0],
-            // Normals for the forward side (towards -z)
-            [0.0, 0.0, -1.0],
-            [0.0, 0.0, -1.0],
-            [0.0, 0.0, -1.0],
-            [0.0, 0.0, -1.0],
-        ],
-    );
+    let mut normals = vec![
+        // Normals for the top side (towards +y)
+        [0.0, 1.0, 0.0],
+        [0.0, 1.0, 0.0],
+        [0.0, 1.0, 0.0],
+        [0.0, 1.0, 0.0],
+        // Normals for the bottom side (towards -y)
+        [0.0, -1.0, 0.0],
+        [0.0, -1.0, 0.0],
+        [0.0, -1.0, 0.0],
+        [0.0, -1.0, 0.0],
+        // Normals for the right side (towards +x)
+        [1.0, 0.0, 0.0],
+        [1.0, 0.0, 0.0],
+        [1.0, 0.0, 0.0],
+        [1.0, 0.0, 0.0],
+        // Normals for the left side (towards -x)
+        [-1.0, 0.0, 0.0],
+        [-1.0, 0.0, 0.0],
+        [-1.0, 0.0, 0.0],
+        [-1.0, 0.0, 0.0],
+        // Normals for the back side (towards +z)
+        [0.0, 0.0, 1.0],
+        [0.0, 0.0, 1.0],
+        [0.0, 0.0, 1.0],
+        [0.0, 0.0, 1.0],
+        // Normals for the forward side (towards -z)
+        [0.0, 0.0, -1.0],
+        [0.0, 0.0, -1.0],
+        [0.0, 0.0, -1.0],
+        [0.0, 0.0, -1.0],
+    ];
 
-    mesh.insert_indices(Indices::U32(vec![
+    let mut indices = vec![
         0,3,1 , 1,3,2, // triangles making up the top (+y) facing side.
         4,5,7 , 5,6,7, // bottom (-y)
         8,11,9 , 9,11,10, // right (+x)
         12,13,15 , 13,14,15, // left (-x)
         16,19,17 , 17,19,18, // back (+z)
         20,21,23 , 21,22,23, // forward (-z)
-    ]));
+    ];
+
+    // remove all the faces that we don't want
+    for face in without_faces {
+        for i in 0..=5 {
+            if face as usize == 0 {
+                indices.remove(0);
+            }
+            else {
+                println!("INDIC{}", face as usize);
+                println!("{}", indices.remove(face as usize * 6));
+            }
+        }
+    }
+
+    mesh.insert_attribute(
+        Mesh::ATTRIBUTE_POSITION,
+        // Each array is an [x, y, z] coordinate in local space.
+        // The camera coordinate space is right-handed x-right, y-up, z-back. This means "forward" is -Z.
+        // Meshes always rotate around their local [0, 0, 0] when a rotation is applied to their Transform.
+        // By centering our mesh around the origin, rotating the mesh preserves its center of mass.
+        positions
+    );
+
+    mesh.insert_attribute(
+        Mesh::ATTRIBUTE_UV_0,
+        uvs
+    );
+
+    mesh.insert_attribute(
+        Mesh::ATTRIBUTE_NORMAL,
+        normals
+    );
+
+    mesh.insert_indices(Indices::U32(indices));
 
     mesh
 }
@@ -191,7 +212,7 @@ fn spawn_terrain(
         Transform::from_xyz(50.0, 8.0, 50.0),
         Collider::cuboid(0.5, 0.5, 0.5),
 
-        Mesh3d(meshes.add(create_cube_mesh([BlockFace::Top]))),
+        Mesh3d(meshes.add(create_cube_mesh([BlockFace::Top, BlockFace::Bottom]))),
         MeshMaterial3d(materials.add(Color::from(HOT_PINK))),
     ));
 }
