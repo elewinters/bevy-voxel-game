8ba7fddee2a1a6d0c0aefd821a6fda5e47a58437
ele <86350819+elewinters@users.noreply.github.com>
2025-07-29 13:03:42 +0200

    seperated noise logic to generate_noise

--- src/worldgen.rs
+++ src/worldgen.rs
@@ -12,6 +12,19 @@ impl Plugin for WorldGenPlugin {
     }
 }
 
+const FLATNESS: f64 = 60.0;
+const SPIKINESS: f64 = 20.0;
+
+/* ------------------ */
+/*      functions     */
+/* ------------------ */
+fn generate_noise(x: f64, y: f64, z: f64) -> f32 {
+    let noise = Perlin::new(512);
+    let noise_y = noise.get([x / FLATNESS , y / FLATNESS , z / FLATNESS]) * SPIKINESS;
+
+    noise_y.round() as f32
+}
+
 /* ---------------- */
 /*      systems     */
 /* ---------------- */
@@ -20,8 +33,6 @@ fn spawn_terrain(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>
 ) {
-    let noise = Perlin::new(512);
-
     let size_x = 32 * 3;
     let size_y = 12;
     let size_z = 32 * 3;
@@ -32,16 +43,11 @@ fn spawn_terrain(
     for x in 0..size_x {
         for y in 0..size_y {
             for z in 0..size_x {
-                let flatness = 60.0; // the higher the number, the less hills there are
-                let spikiness = 20.0; // the higher the number, the spikier hills get
-
-                let noise_y = noise.get([x as f64 / flatness , y as f64 / flatness , z as f64 / flatness]) as f32 * spikiness;           
-
                 if x == 0 || x == size_x - 1 ||
                    y == 0 || y == size_y - 1 ||
                    z == 0 || z == size_z - 1 {
                     commands.spawn((
-                        Transform::from_xyz(x as f32, noise_y.round(), z as f32),
+                        Transform::from_xyz(x as f32, generate_noise(x as f64, y as f64, z as f64), z as f32),
                         Collider::cuboid(0.5, 0.5, 0.5),
 
                         Mesh3d(mesh.clone()),
