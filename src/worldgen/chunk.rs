7fa6a7bbbc1f3278b605904c384e2ad308a98fa3
ele <86350819+elewinters@users.noreply.github.com>
2025-07-30 17:45:57 +0200

    seperated chunk logic into its own file

--- src/worldgen/chunk.rs
+++ src/worldgen/chunk.rs
@@ -8,10 +8,9 @@ use noise::*;
 
 use crate::player;
 
-pub struct WorldGenPlugin;
-impl Plugin for WorldGenPlugin {
+pub struct ChunkPlugin;
+impl Plugin for ChunkPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, spawn_atmosphere);
         app.add_systems(Update, (
             update_current_chunk, 
             spawn_chunks,
@@ -202,29 +201,6 @@ fn despawn_chunks(
             commands.entity(entity).despawn();
         }
     }
-}
-
-fn spawn_atmosphere(mut commands: Commands) {
-    // blue sky
-    commands.insert_resource(ClearColor(Color::linear_rgb(0.83, 0.96, 0.96)));
-
-    // some ambient light
-    commands.insert_resource(AmbientLight {
-        color: Color::WHITE,
-        brightness: 10_000.0,
-        affects_lightmapped_meshes: true,
-    });
-
-    // the sun
-    commands.spawn((
-        DirectionalLight {
-            illuminance: light_consts::lux::FULL_DAYLIGHT,
-            shadows_enabled: true,
-            ..default()
-        },
-
-        Transform::from_xyz(0.0, 10_000.0, 0.0).looking_at(Vec3::ZERO, Vec3::Y),
-    ));
 }
 
 fn update_current_chunk(
---
+++ src/worldgen/mod.rs
@@ -0,0 +1,34 @@
+use bevy::prelude::*;
+
+mod chunk;
+
+pub struct WorldGenPlugin;
+impl Plugin for WorldGenPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, spawn_atmosphere);
+        app.add_plugins(chunk::ChunkPlugin);
+    }
+}
+
+fn spawn_atmosphere(mut commands: Commands) {
+    // blue sky
+    commands.insert_resource(ClearColor(Color::linear_rgb(0.83, 0.96, 0.96)));
+
+    // some ambient light
+    commands.insert_resource(AmbientLight {
+        color: Color::WHITE,
+        brightness: 10_000.0,
+        affects_lightmapped_meshes: true,
+    });
+
+    // the sun
+    commands.spawn((
+        DirectionalLight {
+            illuminance: light_consts::lux::FULL_DAYLIGHT,
+            shadows_enabled: true,
+            ..default()
+        },
+
+        Transform::from_xyz(0.0, 10_000.0, 0.0).looking_at(Vec3::ZERO, Vec3::Y),
+    ));
+}
