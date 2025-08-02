9790729373134fde5a0f813deebcc2b1856408a2
ele <86350819+elewinters@users.noreply.github.com>
2025-08-02 21:44:45 +0200

    got rid of create_cube_mesh, we just use Cuboid::new instead

--- src/worldgen/chunk.rs
+++ src/worldgen/chunk.rs
@@ -117,119 +117,6 @@ fn generate_noise(perlin_noise: &Perlin, x: f64, y: f64, z: f64) -> f32 {
     noise_y.round() as f32
 }
 
-fn create_cube_mesh() -> Mesh {
-    let mut mesh = Mesh::new(PrimitiveTopology::TriangleList, RenderAssetUsages::RENDER_WORLD);
-
-    let positions = vec![
-        // top (facing towards +y)
-        [-0.5, 0.5, -0.5], // vertex with index 0
-        [0.5, 0.5, -0.5], // vertex with index 1
-        [0.5, 0.5, 0.5], // etc. until 23
-        [-0.5, 0.5, 0.5],
-        // bottom   (-y)
-        [-0.5, -0.5, -0.5],
-        [0.5, -0.5, -0.5],
-        [0.5, -0.5, 0.5],
-        [-0.5, -0.5, 0.5],
-        // right    (+x)
-        [0.5, -0.5, -0.5],
-        [0.5, -0.5, 0.5],
-        [0.5, 0.5, 0.5], // This vertex is at the same position as vertex with index 2, but they'll have different UV and normal
-        [0.5, 0.5, -0.5],
-        // left     (-x)
-        [-0.5, -0.5, -0.5],
-        [-0.5, -0.5, 0.5],
-        [-0.5, 0.5, 0.5],
-        [-0.5, 0.5, -0.5],
-        // back     (+z)
-        [-0.5, -0.5, 0.5],
-        [-0.5, 0.5, 0.5],
-        [0.5, 0.5, 0.5],
-        [0.5, -0.5, 0.5],
-        // forward  (-z)
-        [-0.5, -0.5, -0.5],
-        [-0.5, 0.5, -0.5],
-        [0.5, 0.5, -0.5],
-        [0.5, -0.5, -0.5],
-    ];
-
-    let uvs = vec![
-        // Assigning the UV coords for the top side.
-        [0.0, 0.2], [0.0, 0.0], [1.0, 0.0], [1.0, 0.2],
-        // Assigning the UV coords for the bottom side.
-        [0.0, 0.45], [0.0, 0.25], [1.0, 0.25], [1.0, 0.45],
-        // Assigning the UV coords for the right side.
-        [1.0, 0.45], [0.0, 0.45], [0.0, 0.2], [1.0, 0.2],
-        // Assigning the UV coords for the left side.
-        [1.0, 0.45], [0.0, 0.45], [0.0, 0.2], [1.0, 0.2],
-        // Assigning the UV coords for the back side.
-        [0.0, 0.45], [0.0, 0.2], [1.0, 0.2], [1.0, 0.45],
-        // Assigning the UV coords for the forward side.
-        [0.0, 0.45], [0.0, 0.2], [1.0, 0.2], [1.0, 0.45],
-    ];
-
-    let normals = vec![
-        // Normals for the top side (towards +y)
-        [0.0, 1.0, 0.0],
-        [0.0, 1.0, 0.0],
-        [0.0, 1.0, 0.0],
-        [0.0, 1.0, 0.0],
-        // Normals for the bottom side (towards -y)
-        [0.0, -1.0, 0.0],
-        [0.0, -1.0, 0.0],
-        [0.0, -1.0, 0.0],
-        [0.0, -1.0, 0.0],
-        // Normals for the right side (towards +x)
-        [1.0, 0.0, 0.0],
-        [1.0, 0.0, 0.0],
-        [1.0, 0.0, 0.0],
-        [1.0, 0.0, 0.0],
-        // Normals for the left side (towards -x)
-        [-1.0, 0.0, 0.0],
-        [-1.0, 0.0, 0.0],
-        [-1.0, 0.0, 0.0],
-        [-1.0, 0.0, 0.0],
-        // Normals for the back side (towards +z)
-        [0.0, 0.0, 1.0],
-        [0.0, 0.0, 1.0],
-        [0.0, 0.0, 1.0],
-        [0.0, 0.0, 1.0],
-        // Normals for the forward side (towards -z)
-        [0.0, 0.0, -1.0],
-        [0.0, 0.0, -1.0],
-        [0.0, 0.0, -1.0],
-        [0.0, 0.0, -1.0],
-    ];
-
-    let indices = vec![
-        0,3,1 , 1,3,2, // triangles making up the top (+y) facing side.
-        4,5,7 , 5,6,7, // bottom (-y)
-        8,11,9 , 9,11,10, // right (+x)
-        12,13,15 , 13,14,15, // left (-x)
-        16,19,17 , 17,19,18, // back (+z)
-        20,21,23 , 21,22,23, // forward (-z)
-    ];
-
-    mesh.insert_attribute(
-        Mesh::ATTRIBUTE_POSITION,
-        positions
-    );
-
-    mesh.insert_attribute(
-        Mesh::ATTRIBUTE_UV_0,
-        uvs
-    );
-
-    mesh.insert_attribute(
-        Mesh::ATTRIBUTE_NORMAL,
-        normals
-    );
-
-    mesh.insert_indices(Indices::U32(indices));
-
-    mesh
-}
-
 /* ---------------- */
 /*      systems     */
 /* ---------------- */
@@ -245,9 +132,9 @@ fn startup(
         z: 0.0
     });
 
-    let mut mesh = create_cube_mesh();
-    let mut mesh2 = create_cube_mesh();
-    
+    let mut mesh = Cuboid::new(1.0, 1.0, 1.0).mesh().build();
+    let mut mesh2 = Cuboid::new(1.0, 1.0, 1.0).mesh().build();
+
     mesh2.translate_by(Vec3::new(1.0, 1.0, 1.0));
     mesh.merge(&mesh2).unwrap();
 
