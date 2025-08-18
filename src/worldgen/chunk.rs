2829993735868aa7511e4188c8897cb0c07e2f54
ele <86350819+elewinters@users.noreply.github.com>
2025-08-18 23:18:35 +0200

    simplified handle_chunk_tasks

--- src/worldgen/chunk.rs
+++ src/worldgen/chunk.rs
@@ -429,32 +429,33 @@ fn handle_chunk_tasks(
 ) {
     // remove tasks from the queue that have finished and have spawned successfully
     queue.0.retain_mut(|task| {
-        if let Some(chunk) = block_on(future::poll_once(task)) {
-            // we push Bundles into this vector because spawn_batch is faster than individually spawning
-            let mut batch = Vec::with_capacity(CHUNK_SIZE_HORIZONTAL as usize);
+        match block_on(future::poll_once(task)) {
+            Some(chunk_data) => {
+                let len = chunk_data.voxel_positions.len();
 
-            for (((voxel_positions, transform), mesh), collider) in chunk.voxel_positions.into_iter().zip(chunk.transforms).zip(chunk.meshes).zip(chunk.colliders) {
-                batch.push(ChunkBundle(
-                    Chunk {
-                        voxel_positions
-                    },
+                // we push Bundles into this vector because spawn_batch is faster than individually spawning
+                let mut chunk_bundles = Vec::with_capacity(len);
 
-                    transform,
-                    collider,
+                for i in 0..len {
+                    chunk_bundles.push(ChunkBundle(
+                        Chunk {
+                            voxel_positions: chunk_data.voxel_positions[i].clone()
+                        },
 
-                    Mesh3d(meshes.add(mesh)),
-                    MeshMaterial3d(materials.add(Color::from(LAWN_GREEN))),
-                ));
+                        chunk_data.transforms[i],
+                        chunk_data.colliders[i].clone(),
+
+                        Mesh3d(meshes.add(chunk_data.meshes[i].clone())),
+                        MeshMaterial3d(materials.add(Color::from(LAWN_GREEN))),
+                    ));
+                }
+                
+                commands.spawn_batch(chunk_bundles);
+
+                // remove from the queue, as the task has finished 
+                false
             }
-
-            commands.spawn_batch(batch);
-
-            // remove from the queue, as the task has finished 
-            false
-        }
-        else {
-            // keep it in the queue, as the task is still processing
-            true
+            None => true, // keep it in the queue, as the task is still processing
         }
     });
 }
