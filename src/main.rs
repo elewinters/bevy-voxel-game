1d936b42f1271c79d3c6c37b358512dec89babc6
ele <86350819+elewinters@users.noreply.github.com>
2025-08-18 18:20:50 +0200

    add TODO to main.rs

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,15 @@ mod player;
 mod window;
 mod worldgen;
 
+/* 
+    TODO:
+        - fix highlight mesh only updating position when you move the mouse (this should be easy to fix)
+        - add some ambient occlusion and mess with the graphics a bit to make the game look a bit better
+        - clean up a lot of the code (especially worldgen/chunk.rs and player/interaction.rs)
+
+        - after all that is done, publish a 0.1.0 release on github 
+*/
+
 /* ------------------ */
 /*      functions     */
 /* ------------------ */
