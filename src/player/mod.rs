1111a587e6db16809ced8bb9ea96ef6188eb9cac
ele <86350819+elewinters@users.noreply.github.com>
2025-08-24 21:44:10 +0200

    moved spawn_player from player/movement.rs to player/mod.rs

--- src/player/mod.rs
+++ src/player/mod.rs
@@ -1,4 +1,7 @@
+use bevy::core_pipeline::bloom::Bloom;
 use bevy::prelude::*;
+use bevy::render::camera::Exposure;
+
 use crate::*;
 
 mod movement;
@@ -7,6 +10,8 @@ mod interaction;
 pub struct PlayerPlugin;
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
+        app.add_systems(Startup, spawn_player);
+
         app.add_plugins(movement::MovementPlugin);
         app.add_plugins(interaction::InteractionPlugin);
     }
@@ -20,4 +25,42 @@ impl Plugin for PlayerPlugin {
 #[derive(Component)]
 pub struct Player {
     fly: bool
+}
+
+/* ---------------- */
+/*      systems     */
+/* ---------------- */
+// #tag systems
+
+fn spawn_player(mut commands: Commands) {
+    commands.spawn((
+        Player { 
+            fly: false 
+        },
+
+        Transform::from_xyz(0.0, 20.0, 0.0),
+        Visibility::default(),
+        Collider::round_cylinder(0.9, 0.3, 0.2),
+
+        KinematicCharacterController {
+            custom_mass: Some(5.0),
+            
+            max_slope_climb_angle: 45.0_f32.to_radians(), // don't allow climbing slopes larger than 45 degrees
+            min_slope_slide_angle: 30.0_f32.to_radians(), // automatically slide down on slopes smaller than 30 degrees
+
+            snap_to_ground: None,
+
+            ..default()
+        },
+
+        // camera
+        children![(
+            Camera3d::default(), 
+            Transform::from_xyz(0.0, 0.2, -0.1),
+            Projection::from(PerspectiveProjection {fov: 90.0_f32.to_radians(),..default()}),
+
+            Exposure::SUNLIGHT,
+            Bloom::NATURAL,
+        )]
+    ));
 }
--- src/player/movement.rs
+++ src/player/movement.rs
@@ -1,7 +1,5 @@
 use bevy::prelude::*;
 use bevy::input::mouse::MouseMotion;
-use bevy::render::camera::Exposure;
-
 use bevy_rapier3d::control::KinematicCharacterController;
 
 use crate::player::*;
@@ -9,8 +7,6 @@ use crate::player::*;
 pub struct MovementPlugin;
 impl Plugin for MovementPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, spawn_player);
-
         app.add_systems(Update, (
             move_player,
             look_player,
@@ -33,36 +29,6 @@ const GRAVITY: f32 = -9.81;
 /*      systems     */
 /* ---------------- */
 // #tag systems
-
-pub fn spawn_player(mut commands: Commands) {
-    commands.spawn((
-        Player { fly: false },
-
-        Transform::from_xyz(0.0, 20.0, 0.0),
-        Visibility::default(),
-        Collider::round_cylinder(0.9, 0.3, 0.2),
-
-        KinematicCharacterController {
-            custom_mass: Some(5.0),
-            
-            max_slope_climb_angle: 45.0_f32.to_radians(), // don't allow climbing slopes larger than 45 degrees
-            min_slope_slide_angle: 30.0_f32.to_radians(), // automatically slide down on slopes smaller than 30 degrees
-
-            snap_to_ground: None,
-
-            ..default()
-        },
-
-        // camera
-        children![(
-            Camera3d::default(), 
-            Transform::from_xyz(0.0, 0.2, -0.1),
-            Projection::from(PerspectiveProjection {fov: 90.0_f32.to_radians(),..default()}),
-
-            Exposure::SUNLIGHT,
-        )]
-    ));
-}
 
 // responsible for moving around with WASD, jumping and applying gravity
 fn move_player(
