44afac9cce930d2e45a08fe338ddfba51a8a3783
ele <86350819+elewinters@users.noreply.github.com>
2025-08-24 21:37:43 +0200

    reduced  AmbientLight brightness

--- src/worldgen/mod.rs
+++ src/worldgen/mod.rs
@@ -17,7 +17,7 @@ fn spawn_atmosphere(mut commands: Commands) {
     // some ambient light
     commands.insert_resource(AmbientLight {
         color: Color::WHITE,
-        brightness: 10_000.0,
+        brightness: 5_000.0,
         affects_lightmapped_meshes: true,
     });
 
