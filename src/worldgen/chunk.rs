8554a7c171316263671c8fb5143cd9c462882b42
ele <86350819+elewinters@users.noreply.github.com>
2025-08-27 21:55:50 +0200

    fixed exponent of valley generation being negative when it should be positive

--- src/worldgen/chunk.rs
+++ src/worldgen/chunk.rs
@@ -63,7 +63,7 @@ const HEIGHT_VARIATION: f32 = 50.0;
 
 const VALLEY_THRESHOLD: f32 = 0.0; // below this value we'll have valleys
 const VALLEY_SMOOTHNESS: f32 = 1.5; // how smooth valleys are
-const VALLEY_STEP: f32 = 0.35; // how much smoother the valleys should get the lower they are, lower values smoothen while higher values roughen
+const VALLEY_STEP: f32 = 3.0; // how much smoother the valleys should get the lower they are
 
 /* -------------- */
 /*      types     */
@@ -193,7 +193,7 @@ fn generate_noise(perlin_noise: &FastNoiseLite, x: f32, z: f32) -> f32 {
     // valley
     let y = if y < VALLEY_THRESHOLD {
         // we multiply VALLEY_SMOOTHNESS by (VALLEY_STEP.powf(y) so that the deeper the valley the smoother it is
-        y * HEIGHT_VARIATION / (VALLEY_SMOOTHNESS * (VALLEY_STEP.powf(y)))
+        y * HEIGHT_VARIATION / (VALLEY_SMOOTHNESS * (VALLEY_STEP.powf(y.abs())))
     }
     // normal terrain
     else {
