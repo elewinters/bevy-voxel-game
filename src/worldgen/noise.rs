7493cdb22a155d1a3165de7bd8c33eda1a00772a
ele <86350819+elewinters@users.noreply.github.com>
2025-08-29 16:30:43 +0200

    added constants for hills variables in noise.rs

--- src/worldgen/noise.rs
+++ src/worldgen/noise.rs
@@ -6,11 +6,14 @@ use fastnoise_lite::FastNoiseLite;
 // #tag constants
 
 pub const FREQUENCY: f32 = 0.006; // essentially the scale of the noise function, lower values zoom in while higher values zoom out
-const HEIGHT_VARIATION: f32 = 10.0;
 
-const VALLEY_THRESHOLD: f32 = 0.0; // below this value we'll have valleys
-const VALLEY_SMOOTHNESS: f32 = 1.5; // how smooth valleys are
-const VALLEY_STEP: f32 = 3.0; // how much smoother the valleys should get the lower they are
+const PLAINS_HEIGHT_VARIATION: f32 = 10.0;
+const PLAINS_VALLEY_THRESHOLD: f32 = 0.0; // below this value we'll have valleys
+const PLAINS_VALLEY_SMOOTHNESS: f32 = 1.5; // how smooth valleys are
+const PLAINS_VALLEY_STEP: f32 = 3.0; // how much smoother the valleys should get the lower they are
+
+const HILLS_HEIGHT_VARIATION: f32 = 50.0;
+const HILLS_WAVELENGTH: f32 = 5.0;
 
 /* ------------------ */
 /*      functions     */
@@ -19,16 +22,16 @@ const VALLEY_STEP: f32 = 3.0; // how much smoother the valleys should get the lo
 
 pub fn generate_noise(perlin_noise: &FastNoiseLite, x: f32, z: f32) -> f32 {
     let plains = perlin_noise.get_noise_2d(x, z);
-    let hills = perlin_noise.get_noise_2d(x / 5.0, z / 5.0) * 50.0;
+    let hills = perlin_noise.get_noise_2d(x / HILLS_WAVELENGTH, z / HILLS_WAVELENGTH) * HILLS_HEIGHT_VARIATION;
 
-    // valley
-    let plains = if plains < VALLEY_THRESHOLD {
+    // plains valleys
+    let plains = if plains < PLAINS_VALLEY_THRESHOLD {
         // we multiply VALLEY_SMOOTHNESS by (VALLEY_STEP.powf(y) so that the deeper the valley the smoother it is
-        plains * HEIGHT_VARIATION / (VALLEY_SMOOTHNESS * (VALLEY_STEP.powf(plains.abs())))
+        plains * PLAINS_HEIGHT_VARIATION / (PLAINS_VALLEY_SMOOTHNESS * (PLAINS_VALLEY_STEP.powf(plains.abs())))
     }
-    // normal terrain
+    // normal plains
     else {
-        (plains * HEIGHT_VARIATION).powf(1.1)
+        (plains * PLAINS_HEIGHT_VARIATION).powf(1.1)
     };
 
     (plains + hills).floor()
