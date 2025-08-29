a7dbcd5e125361810be5cc8dcf4a9e239ebb137a
ele <86350819+elewinters@users.noreply.github.com>
2025-08-29 17:04:18 +0200

    update TODO

--- src/main.rs
+++ src/main.rs
@@ -10,15 +10,9 @@ pub mod window;
 
 /* 
     TODO:
-        - fix highlight mesh only updating position when you move the mouse (this should be easy to fix)
-        - add some ambient occlusion and mess with the graphics a bit to make the game look a bit better
-        - clean up a lot of the code (especially worldgen/chunk.rs and player/interaction.rs)
-
-        MAYBE (i can always do this after 0.1.0):
-            - chunk serialization
-            - multiple block types
-
-        - after all that is done, publish a 0.1.0 release on github 
+        - structures (trees, flowers, etc. randomly spawning in the world)
+        - multiple block types
+        - mountains
 */
 
 /* ------------------ */
