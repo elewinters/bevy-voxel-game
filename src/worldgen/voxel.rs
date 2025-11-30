a4dc7dab97a6d4790b4f0141ffa88d98d6d95183
ele <86350819+elewinters@users.noreply.github.com>
2025-11-30 13:26:56 +0100

    never draw bottom faces as they cannot be seen as of this time

--- src/worldgen/voxel.rs
+++ src/worldgen/voxel.rs
@@ -152,6 +152,10 @@ pub fn voxel_mesh(faces: Vec<VoxelFace>) -> Mesh {
 
 // uses an IVec so that it can be hashed properly
 pub fn should_draw_face(face: &VoxelFace, voxel_pos: &IVec3, voxel_positions: &HashSet<IVec3>) -> bool {
+    if let VoxelFace::Bottom = face {
+        return false;
+    }
+    
     let neighbor_pos = match face {
         VoxelFace::Front => voxel_pos + IVec3::new(0, 0, 1),
         VoxelFace::Back => voxel_pos + IVec3::new(0, 0, -1),
